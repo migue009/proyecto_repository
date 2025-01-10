44a2185c081eb4cf4c6908fc2eae6dbce43aa14e
Sam242005 <viverossebastian617@gmail.com>
2025-01-09 22:21:44 -0500

    cambios header

--- APPorte/view/partials/header.php
+++ APPorte/view/partials/header.php
@@ -22,6 +22,7 @@
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.0/dist/sweetalert2.all.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
   <script src="assets/js/plugin/webfont/webfont.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
   <script>
       WebFont.load({
         google: { families: ["Public Sans:300,400,500,600,700"] },
