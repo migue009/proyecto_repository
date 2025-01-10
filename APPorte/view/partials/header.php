820382a29b77326538516bd8a3e7629cd8276bea
Sam242005 <viverossebastian617@gmail.com>
2025-01-09 22:13:52 -0500

    cambios seguros

--- APPorte/view/partials/header.php
+++ APPorte/view/partials/header.php
@@ -16,14 +16,12 @@
   <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
   <link rel="stylesheet" href="assets/css/plugins.min.css" />
   <link rel="stylesheet" href="assets/css/demo.css" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="../mapa/misc/img/dc.css">
   
   <script src="../mapa/misc/lib/mscross-1.1.9.js" type="text/javascript"></script>
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.0/dist/sweetalert2.all.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
   <script src="assets/js/plugin/webfont/webfont.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
   <script>
       WebFont.load({
         google: { families: ["Public Sans:300,400,500,600,700"] },
